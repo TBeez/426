de6041a1ffe0d905c5a0075fbe493f89adf8dfb6
Justin Baldwin <jtbaldwi@live.unc.edu>
2015-12-07 14:57:37 -0500

    Trying new way of updating

--- actions/login.js
+++ actions/login.js
@@ -2,12 +2,10 @@ export const LOG_IN = 'LOG_IN'
 export const LOG_OUT = 'LOG_OUT'
 
 export function login(idToken) {
+    callPHP(idToken);
   return {
     type: LOG_IN,
     idToken
-    
-    window.location.href = 
-      "http://wwwp.cs.unc.edu/Courses/comp426-f15/users/jtbaldwi/Codiad/workspace/cs426/finalProject/login.php?idToken=" + idToken;
   }
 }
 
@@ -15,4 +13,12 @@ export function logout() {
   return {
     type: LOG_OUT
   }
+}
+function callPHP(idToken) {
+  var httpc = new XMLHttpRequest();
+  var url = "http://wwwp.cs.unc.edu/Courses/comp426-f15/users/jtbaldwi/Codiad/workspace/cs426/finalProject/login.php";
+  httpc.open("POST", url, true); //send a post request to PHP code
+  httpc.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  httpc.setRequestHeader("Content-Length", idToken.length);
+  httpc.send(idToken);
 }
