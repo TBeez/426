f47a47405bea39d7669b84774e53e96b9e940d6a
Justin Baldwin <jtbaldwi@live.unc.edu>
2015-12-07 15:03:55 -0500

    Update login.js

--- actions/login.js
+++ actions/login.js
@@ -2,7 +2,8 @@ export const LOG_IN = 'LOG_IN'
 export const LOG_OUT = 'LOG_OUT'
 
 export function login(idToken) {
-    callPHP(idToken);
+    $url = "http://wwwp.cs.unc.edu/Courses/comp426-f15/users/jtbaldwi/Codiad/workspace/cs426/finalProject/login.php";
+    $.get($url, {idToken: idToken}); //Send idToken to PHP code
   return {
     type: LOG_IN,
     idToken
@@ -13,12 +14,4 @@ export function logout() {
   return {
     type: LOG_OUT
   }
-}
-function callPHP(idToken) {
-  var httpc = new XMLHttpRequest();
-  var url = "http://wwwp.cs.unc.edu/Courses/comp426-f15/users/jtbaldwi/Codiad/workspace/cs426/finalProject/login.php";
-  httpc.open("POST", url, true); //send a post request to PHP code
-  httpc.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  httpc.setRequestHeader("Content-Length", idToken.length);
-  httpc.send(idToken);
 }
