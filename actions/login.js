7543e8f1c11043e897f1b5d522ce51d0d96e89dd
Justin Baldwin <jtbaldwi@live.unc.edu>
2015-12-07 15:12:54 -0500

    Update login.js

--- actions/login.js
+++ actions/login.js
@@ -2,8 +2,6 @@ export const LOG_IN = 'LOG_IN'
 export const LOG_OUT = 'LOG_OUT'
 
 export function login(idToken) {
-    $url = "http://wwwp.cs.unc.edu/Courses/comp426-f15/users/jtbaldwi/Codiad/workspace/cs426/finalProject/login.php";
-    $.get($url, {idToken: idToken}); //Send idToken to PHP code
   return {
     type: LOG_IN,
     idToken
